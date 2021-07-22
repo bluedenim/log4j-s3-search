c4e7bb7b4a9f748e79842293ed4bf3ea0dcf5cf6
Davin Chia <davinchia@gmail.com>
2021-07-22 23:05:42 +0800

    Revert all local changes.

--- appender-core/src/main/java/com/van/logging/aws/S3PublishHelper.java
+++ appender-core/src/main/java/com/van/logging/aws/S3PublishHelper.java
@@ -1,22 +1,10 @@
 package com.van.logging.aws;
 
-import com.amazonaws.AmazonClientException;
-import com.amazonaws.AmazonServiceException;
-import com.amazonaws.ClientConfiguration;
-import com.amazonaws.auth.AWSCredentials;
-import com.amazonaws.auth.AWSStaticCredentialsProvider;
-import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.client.builder.AwsClientBuilder;
-import com.amazonaws.regions.Regions;
-import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
-import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.model.CannedAccessControlList;
-import com.amazonaws.services.s3.model.GetObjectRequest;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.amazonaws.services.s3.model.PutObjectResult;
-import com.amazonaws.services.s3.model.S3Object;
 import com.van.logging.AbstractFilePublishHelper;
 import com.van.logging.IStorageDestinationAdjuster;
 import com.van.logging.PublishContext;
@@ -124,51 +112,6 @@ public class S3PublishHelper extends AbstractFilePublishHelper {
             // System.out.println(String.format("Content MD5: %s", result.getContentMd5()));
         } catch (Exception ex) {
             throw new RuntimeException(String.format("Cannot publish to S3: %s", ex.getMessage()), ex);
-        }
-    }
-
-    public static void main(String[] args) {
-        AWSCredentials credentials = new BasicAWSCredentials("minio", "minio123");
-        ClientConfiguration clientConfiguration = new ClientConfiguration();
-        clientConfiguration.setSignerOverride("AWSS3V4SignerType");
-
-        AmazonS3 s3Client = AmazonS3ClientBuilder
-            .standard()
-            .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration("http://localhost:9000", Regions.US_EAST_1.name()))
-            .withPathStyleAccessEnabled(true)
-            .withClientConfiguration(clientConfiguration)
-            .withCredentials(new AWSStaticCredentialsProvider(credentials))
-            .build();
-
-
-        s3Client = buildClient("minio", "minio123",
-            null,  null, "http://localhost:9000", "us-east-2", true);
-        try {
-            String bucketName = "test-2";
-            String keyName = "minio-sa";
-            System.out.println("Uploading a new object to S3 from a file\n");
-            File file = new File("/Users/davinchia/Desktop/minio-standalone.yml");
-            // Upload file
-            s3Client.putObject(new PutObjectRequest(bucketName, keyName, file));
-
-            // Download file
-            GetObjectRequest rangeObjectRequest = new GetObjectRequest(bucketName, keyName);
-            S3Object objectPortion = s3Client.getObject(rangeObjectRequest);
-            System.out.println("Printing bytes retrieved:");
-        } catch (AmazonServiceException ase) {
-            System.out.println("Caught an AmazonServiceException, which " + "means your request made it "
-                + "to Amazon S3, but was rejected with an error response" + " for some reason.");
-            System.out.println("Error Message:    " + ase.getMessage());
-            System.out.println("HTTP Status Code: " + ase.getStatusCode());
-            System.out.println("AWS Error Code:   " + ase.getErrorCode());
-            System.out.println("Error Type:       " + ase.getErrorType());
-            System.out.println("Request ID:       " + ase.getRequestId());
-
-        } catch (AmazonClientException ace) {
-            System.out.println("Caught an AmazonClientException, which " + "means the client encountered " + "an internal error while trying to "
-                + "communicate with S3, " + "such as not being able to access the network.");
-            System.out.println("Error Message: " + ace.getMessage());
-
         }
     }
 }
