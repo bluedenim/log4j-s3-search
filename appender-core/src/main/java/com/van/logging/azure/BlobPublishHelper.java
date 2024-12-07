44e5a728ca508208fa48aa2a75380373ee8bc200
bluedenim <vancly@hotmail.com>
2024-12-06 18:17:20 -0800

    Switch from com.microsoft.azure:azure-storage to com.azure:azure-storage-blob because that's what they did. Sticking to using connection string auth for now.

--- appender-core/src/main/java/com/van/logging/azure/BlobPublishHelper.java
+++ appender-core/src/main/java/com/van/logging/azure/BlobPublishHelper.java
@@ -1,8 +1,13 @@
 package com.van.logging.azure;
 
-import com.microsoft.azure.storage.CloudStorageAccount;
-import com.microsoft.azure.storage.OperationContext;
-import com.microsoft.azure.storage.blob.*;
+import com.azure.storage.blob.BlobClient;
+import com.azure.storage.blob.BlobContainerClient;
+import com.azure.storage.blob.BlobServiceClient;
+import com.azure.storage.blob.BlobServiceClientBuilder;
+import com.azure.storage.blob.models.AccessTier;
+import com.azure.storage.blob.models.BlobHttpHeaders;
+import com.azure.storage.blob.models.BlobRequestConditions;
+import com.azure.storage.blob.models.ParallelTransferOptions;
 import com.van.logging.AbstractFilePublishHelper;
 import com.van.logging.IStorageDestinationAdjuster;
 import com.van.logging.PublishContext;
@@ -11,6 +16,8 @@ import com.van.logging.utils.PublishHelperUtils;
 import com.van.logging.utils.StringUtils;
 
 import java.io.File;
+import java.time.Duration;
+import java.util.Map;
 
 
 /**
@@ -48,11 +55,14 @@ public class BlobPublishHelper extends AbstractFilePublishHelper {
         if (null == connectionString || connectionString.isEmpty()) {
             connectionString = System.getenv("AZURE_STORAGE_CONNECTION_STRING");
         }
-        CloudStorageAccount storageAccount = CloudStorageAccount.parse(connectionString);
-        CloudBlobClient blobClient = storageAccount.createCloudBlobClient();
-        CloudBlobContainer container = blobClient.getContainerReference(blobConfiguration.getContainerName());
-        boolean created = container.createIfNotExists(
-            BlobContainerPublicAccessType.CONTAINER, new BlobRequestOptions(), new OperationContext());
+        // Source: https://learn.microsoft.com/en-us/azure/storage/blobs/storage-quickstart-blobs-java#upload-blobs-to-a-container
+        BlobServiceClient blobServiceClient = new BlobServiceClientBuilder()
+            .connectionString(connectionString)
+            .buildClient();
+
+        BlobContainerClient blobContainerClient = blobServiceClient.createBlobContainerIfNotExists(
+            blobConfiguration.getContainerName()
+        );
 
         String path = StringUtils.addTrailingIfNeeded(
             PublishHelperUtils.adjustStoragePathIfNecessary(
@@ -66,14 +76,20 @@ public class BlobPublishHelper extends AbstractFilePublishHelper {
             VansLogger.logger.debug(String.format("Publishing %s to Azure blob (container=%s; blob=%s):",
                 file.getAbsolutePath(), blobConfiguration.getContainerName(), blobName));
         }
-
-        CloudBlockBlob blob = container.getBlockBlobReference(blobName);
+        BlobClient blobClient = blobContainerClient.getBlobClient(blobName);
+        BlobHttpHeaders headers = new BlobHttpHeaders();
         if (this.blobConfiguration.isCompressionEnabled()) {
-            blob.getProperties().setContentType("application/gzip");
+            headers.setContentType("application/gzip");
         } else {
-            blob.getProperties().setContentType("text/plain");
+            headers.setContentType("text/plain");
         }
-        blob.uploadFromFile(file.getAbsolutePath());
+        // The following is a bit odd by setting almost everything as null except the headers, but that
+        // is what BlobClient.uploadFromFile(String) does.
+        blobClient.uploadFromFile(
+            file.getAbsolutePath(), (ParallelTransferOptions)null, headers,
+            (Map<String,String>)null, (AccessTier)null, (BlobRequestConditions)null, (Duration)null
+        );
+
         if (this.verbose) {
             VansLogger.logger.debug(String.format("Publishing to Azure blob %s done.", blobName));
         }
